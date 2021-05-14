8b654892144cb405281b50dfeaeda075a2d88c65
Leonid Levkin <llevkin@yandex.ru>
2021-05-14 13:51:42 +0300

    test(tools): **define** tests update

--- packages/@biorate/tools/tests/__mocks__/define.ts
+++ packages/@biorate/tools/tests/__mocks__/define.ts
@@ -4,5 +4,5 @@ import { IDefine } from '../../interfaces';
 export function prop(mods: IDefine.Mods, field = 'a', value = 1) {
   const obj = {};
   define.prop(obj, field, value, mods);
-  return obj;
+  return Object.getOwnPropertyDescriptor(obj, 'a');
 }
--- packages/@biorate/tools/tests/define.spec.ts
+++ packages/@biorate/tools/tests/define.spec.ts
@@ -1,42 +1,35 @@
 import { assert } from 'chai';
+import { IDefine } from '../interfaces';
 // import { define } from '../src';
 import { prop } from './__mocks__/define';
 
 describe('define', () => {
-  it('define prop [configurable]', () => {
-    assert(Object.getOwnPropertyDescriptor(prop('c'), 'a').configurable);
-  });
-
-  it('define prop [enumerable]', () => {
-    assert(Object.getOwnPropertyDescriptor(prop('e'), 'a').enumerable);
-  });
-
-  it('define prop [writable]', () => {
-    assert(Object.getOwnPropertyDescriptor(prop('w'), 'a').writable);
-  });
+  for (const property of ['configurable', 'enumerable', 'writable'])
+    it(`define prop [${property}]`, () =>
+      assert(prop(property[0] as unknown as IDefine.Mods)[property]));
 
   it('define prop [configurable & writable]', () => {
-    const object = prop('ce');
-    assert(Object.getOwnPropertyDescriptor(object, 'a').configurable);
-    assert(Object.getOwnPropertyDescriptor(object, 'a').enumerable);
+    const descriptor = prop('ce');
+    assert(descriptor.configurable);
+    assert(descriptor.enumerable);
   });
 
   it('define prop [enumerable & writable]', () => {
-    const object = prop('we');
-    assert(Object.getOwnPropertyDescriptor(object, 'a').writable);
-    assert(Object.getOwnPropertyDescriptor(object, 'a').enumerable);
+    const descriptor = prop('we');
+    assert(descriptor.writable);
+    assert(descriptor.enumerable);
   });
 
   it('define prop [configurable & enumerable]', () => {
-    const object = prop('ce');
-    assert(Object.getOwnPropertyDescriptor(object, 'a').configurable);
-    assert(Object.getOwnPropertyDescriptor(object, 'a').enumerable);
+    const descriptor = prop('ce');
+    assert(descriptor.configurable);
+    assert(descriptor.enumerable);
   });
 
   it('define prop [configurable & enumerable & writable]', () => {
-    const object = prop('cwe');
-    assert(Object.getOwnPropertyDescriptor(object, 'a').configurable);
-    assert(Object.getOwnPropertyDescriptor(object, 'a').enumerable);
-    assert(Object.getOwnPropertyDescriptor(object, 'a').writable);
+    const descriptor = prop('cwe');
+    assert(descriptor.configurable);
+    assert(descriptor.enumerable);
+    assert(descriptor.writable);
   });
 });
