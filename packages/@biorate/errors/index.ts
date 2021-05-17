4ac3b8a08f92cfc04c8bb437091467a6323165c4
Leonid Levkin <llevkin@yandex.ru>
2021-05-17 21:19:47 +0300

    doc(errors, symbolic): examples added

--- packages/@biorate/errors/index.ts
+++ packages/@biorate/errors/index.ts
@@ -1,5 +1,32 @@
 import { format } from 'util';
 
+/**
+ * @description
+ * This module provides a base class for creating error classes with error code (class name),
+ * message template, meta data support out of the box.
+ *
+ * @example
+ * ```ts
+ * import { BaseError } from '@biorate/errors';
+ *
+ * export class MyAwesomeError extends BaseError {
+ *   constructor(args?: any[], meta?: any) {
+ *     super(`Oops... Some error happen, at [%s], in [%s]`, args, meta);
+ *   }
+ * }
+ *
+ * const e = new MyAwesomeError([new Date(), 'core'], { hello: 'world!' });
+ *
+ * console.log(e.meta); // { hello: 'world!' }
+ * console.log(e.code); // MyAwesomeError
+ *
+ * throw e;
+ *           ^
+ * MyAwesomeError: Oops... Some error happen, at [2021-05-13T09:19:22.511Z], in [core]
+ *     at Object.<anonymous> (..core/packages/@biorate/errors/index.ts:28:11)
+ *     at Module._compile (internal/modules/cjs/loader.js:1138:30)
+ * ```
+ */
 export class BaseError extends Error {
   #meta: any = null;
 
--- packages/@biorate/symbolic/index.ts
+++ packages/@biorate/symbolic/index.ts
@@ -1,3 +1,25 @@
+/**
+ * @description
+ * Symbols factory registry with namespaces
+ *
+ * @example
+ * ```ts
+ * import { create } from '@biorate/symbolic';
+ *
+ * const Namespace1 = create('Namespace1');
+ * const Namespace2 = create('Namespace2');
+ *
+ * // auto-create symbol on property call
+ * console.log(Namespace1.Test1); // Symbol(Namespace1.Test)
+ * console.log(Namespace1.Test1 === Namespace1.Test1); // true
+ *
+ * console.log(Namespace1.Test2); // Symbol(Namespace1.Test)
+ * console.log(Namespace1.Test1 === Namespace1.Test2); // false
+ *
+ * // namespace isolation
+ * console.log(Namespace2.Test1 === Namespace1.Test1); // false
+ * ```
+ */
 export function create(label: string) {
   return new Proxy(
     {},
