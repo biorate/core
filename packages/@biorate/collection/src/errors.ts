5e101f0f70cfe29f46a9e329784480d71f5933c6
Leonid Levkin <llevkin@yandex.ru>
2021-09-23 09:35:51 +0300

    refactor(collections): constructor access fix

--- packages/@biorate/collection/src/errors.ts
+++ packages/@biorate/collection/src/errors.ts
@@ -1,7 +1,7 @@
 import { BaseError } from '@biorate/errors';
 
 export class CollectionListItemAlreadyExistsError extends BaseError {
-  constructor(name: string, key: string) {
+  public constructor(name: string, key: string) {
     super('Item [%s] with key [%s] already exists!', [name, key]);
   }
 }
