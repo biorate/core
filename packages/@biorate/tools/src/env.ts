7a1f21e068c6c2e9c00d020af9b883d627e27075
Leonid Levkin <llevkin@yandex.ru>
2021-09-27 10:42:32 +0300

    doc(tools): rebuild

--- packages/@biorate/tools/src/env.ts
+++ packages/@biorate/tools/src/env.ts
@@ -1,4 +1,8 @@
 'use strict';
 const globalThis = Function('return this')();
 
+/**
+ * @description
+ * Is server or browser verification
+ */
 export const isServer = globalThis.window !== globalThis;
