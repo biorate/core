af7b0a0dd34512ca815ed0b28760908043f6514a
Leonid Levkin <llevkin@yandex.ru>
2021-10-16 11:28:35 +0300

    fix(inversion): cache symbols in global object. Fix multi packages bug

--- packages/@biorate/inversion/src/labels.ts
+++ packages/@biorate/inversion/src/labels.ts
@@ -1,4 +1,10 @@
 import { create } from '@biorate/symbolic';
+import { env } from '@biorate/tools';
 
-export const Types = create('Types');
-export const Metadata = create('Metadata');
+const key = Symbol.for('@biorate/inversion.namespace');
+
+if (!env.globalThis[key])
+  env.globalThis[key] = { Types: create('Types'), Metadata: create('Metadata') };
+
+export const Types = env.globalThis[key].Types;
+export const Metadata = env.globalThis[key].Metadata;
