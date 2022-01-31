cc6cb96e8d88a8664820e9e2371d37127562dcc3
Leonid Levkin <llevkin@yandex.ru>
2022-01-31 18:57:03 +0300

    fix(lifecycled): index.d.ts types fix

--- packages/@biorate/lifecycled/index.d.ts
+++ packages/@biorate/lifecycled/index.d.ts
@@ -1,5 +1,10 @@
 declare module '@biorate/lifecycled' {
-  export function lifecycled();
+  export function lifecycled(
+    root: {},
+    onInit?: (object: {}) => {},
+    onKill?: (object: {}) => {},
+  );
   export function init();
   export function kill();
+  export function on(event: string);
 }
