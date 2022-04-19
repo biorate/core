f496007a19020a3f92f2d22d39d0edc77f85ff07
Leonid Levkin <llevkin@yandex.ru>
2022-04-19 09:31:46 +0300

    fix: dependency fix

--- packages/@biorate/react-virtual-table/src/store/base.ts
+++ packages/@biorate/react-virtual-table/src/store/base.ts
@@ -3,6 +3,7 @@ import { ObservableItem, action } from '@biorate/collection';
 export class Base<P = null> extends ObservableItem<P> {
   @action() set(data: Record<string, any>) {
     for (const field in data) if (field in this) this[field] = data[field];
+    return this;
   }
 }
 
