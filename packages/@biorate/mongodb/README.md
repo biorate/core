ce5b2e4f0eda79b002ff653c7f4e3d669df568cf
Leonid Levkin <llevkin@yandex.ru>
2022-04-25 21:14:19 +0300

    docs(mongodb): update

--- packages/@biorate/mongodb/README.md
+++ packages/@biorate/mongodb/README.md
@@ -5,6 +5,84 @@ Mongodb connector
 ### Examples:
 
 ```ts
+import { inject, container, Types, Core } from '@biorate/inversion';
+import { IConfig, Config } from '@biorate/config';
+import {
+  Severity,
+  modelOptions,
+  Prop,
+  MongoDBConnector,
+  IMongoDBConnector,
+  model,
+  ReturnModelType,
+} from '@biorate/mongodb';
+
+// Define models
+@modelOptions({
+  options: {
+    allowMixed: Severity.ALLOW,
+  },
+  schemaOptions: { collection: 'test', versionKey: false },
+})
+export class TestModel {
+  @Prop()
+  firstName: string;
+
+  @Prop()
+  lastName: string;
+
+  @Prop()
+  age: number;
+}
+
+// Define root
+export class Root extends Core() {
+  @inject(MongoDBConnector) public connector: IMongoDBConnector;
+  @model(TestModel) public test: ReturnModelType<typeof TestModel>;
+}
+
+// Bind dependencies
+container.bind<IConfig>(Types.Config).to(Config).inSingletonScope();
+container.bind<IMongoDBConnector>(MongoDBConnector).toSelf().inSingletonScope();
+container.bind<Root>(Root).toSelf().inSingletonScope();
+
+// Configure
+container.get<IConfig>(Types.Config).merge({
+  MongoDB: [
+    {
+      name: 'connection',
+      host: 'mongodb://localhost:27017/',
+      options: {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        dbName: 'test',
+      },
+    },
+  ],
+});
+
+(async () => {
+  const root = container.get<Root>(Root);
+  await root.$run();
+  await root.connector.connection().dropDatabase();
+
+  const connection = root.connector.connection('connection'); // Get connection instance
+  console.log(connection);
+
+  await new root.test({
+    firstName: 'Vasya',
+    lastName: 'Pupkin',
+    age: 36,
+  }).save(); // insert data into test collection
+
+  // Get data from database
+  const data = await root.test.find({ firstName: 'Vasya' }, { _id: 0 });
+  console.log(data); // {
+                     //   firstName: 'Vasya',
+                     //   lastName: 'Pupkin',
+                     //   age: 36,
+                     // }
+})();
 ```
 
 ### Learn
--- packages/@biorate/mongodb/src/index.ts
+++ packages/@biorate/mongodb/src/index.ts
@@ -20,12 +20,95 @@ export * from './interfaces';
  *
  * @example
  * ```
+ * import { inject, container, Types, Core } from '@biorate/inversion';
+ * import { IConfig, Config } from '@biorate/config';
+ * import {
+ *   Severity,
+ *   modelOptions,
+ *   Prop,
+ *   MongoDBConnector,
+ *   IMongoDBConnector,
+ *   model,
+ *   ReturnModelType,
+ * } from '@biorate/mongodb';
+ *
+ * // Define models
+ * @modelOptions({
+ *   options: {
+ *     allowMixed: Severity.ALLOW,
+ *   },
+ *   schemaOptions: { collection: 'test', versionKey: false },
+ * })
+ * export class TestModel {
+ *   @Prop()
+ *   firstName: string;
+ *
+ *   @Prop()
+ *   lastName: string;
+ *
+ *   @Prop()
+ *   age: number;
+ * }
+ *
+ * // Define root
+ * export class Root extends Core() {
+ *   @inject(MongoDBConnector) public connector: IMongoDBConnector;
+ *   @model(TestModel) public test: ReturnModelType<typeof TestModel>;
+ * }
+ *
+ * // Bind dependencies
+ * container.bind<IConfig>(Types.Config).to(Config).inSingletonScope();
+ * container.bind<IMongoDBConnector>(MongoDBConnector).toSelf().inSingletonScope();
+ * container.bind<Root>(Root).toSelf().inSingletonScope();
+ *
+ * // Configure
+ * container.get<IConfig>(Types.Config).merge({
+ *   MongoDB: [
+ *     {
+ *       name: 'connection',
+ *       host: 'mongodb://localhost:27017/',
+ *       options: {
+ *         useNewUrlParser: true,
+ *         useUnifiedTopology: true,
+ *         dbName: 'test',
+ *       },
+ *     },
+ *   ],
+ * });
+ *
+ * (async () => {
+ *   const root = container.get<Root>(Root);
+ *   await root.$run();
+ *   await root.connector.connection().dropDatabase();
+ *
+ *   const connection = root.connector.connection('connection'); // Get connection instance
+ *   console.log(connection);
+ *
+ *   await new root.test({
+ *     firstName: 'Vasya',
+ *     lastName: 'Pupkin',
+ *     age: 36,
+ *   }).save(); // insert data into test collection
+ *
+ *   // Get data from database
+ *   const data = await root.test.find({ firstName: 'Vasya' }, { _id: 0 });
+ *   console.log(data); // {
+ *                      //   firstName: 'Vasya',
+ *                      //   lastName: 'Pupkin',
+ *                      //   age: 36,
+ *                      // }
+ * })();
  * ```
  */
 @injectable()
 export class MongoDBConnector extends Connector<IMongoDBConfig, IMongoDBConnection> {
+  /**
+   * @description Namespace path for fetching configuration
+   */
   protected readonly namespace = 'MongoDB';
-
+  /**
+   * @description Create connection
+   */
   protected async connect(config: IMongoDBConfig) {
     let connection;
     try {
@@ -36,15 +119,21 @@ export class MongoDBConnector extends Connector<IMongoDBConfig, IMongoDBConnecti
     }
     return connection;
   }
-
+  /**
+   * @description Initialize method
+   */
   @init() protected async initialize() {
     connections = this.connections;
     await super.initialize();
   }
 }
-
+/**
+ * @description Private connections link
+ */
 let connections: Map<string, IMongoDBConnection> = null;
-
+/**
+ * @description Model injection decorator
+ */
 export const model = <T = AnyParamConstructor<unknown>>(
   Model: AnyParamConstructor<T>,
   connection?: string,
