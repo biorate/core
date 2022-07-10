ac340e3eec4dc5c6649adf0fddce92ff131f1a93
Leonid Levkin <llevkin@yandex.ru>
2022-07-10 11:08:14 +0300

    fix: react-virtual-table temprorary removed

--- packages/@biorate/react-virtual-table/src/store/bounds.ts
+++
@@ -1,7 +0,0 @@
-import { Base, embed, observable } from './base';
-import { IReactVirtualTable } from '../../interfaces';
-
-export class Bounds extends Base implements IReactVirtualTable.Bounds {
-  @observable() @embed(Bounds.Int) public width = 0;
-  @observable() @embed(Bounds.Int) public height = 0;
-}
