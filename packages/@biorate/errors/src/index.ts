6725dc479228c41fe66876a5892c696dd5b73599
Leonid Levkin <llevkin@yandex.ru>
2022-08-11 13:46:58 +0300

    fix(errors): move code into src

---
+++ packages/@biorate/errors/src/index.ts
@@ -0,0 +1,53 @@
+import { format } from 'util';
+
+/**
+ * @description
+ * This module provides a base class for creating error classes with error code (class name),
+ * message template, meta data support out of the box.
+ *
+ * @example
+ * ```ts
+ * import { BaseError } from '@biorate/errors';
+ *
+ * export class MyAwesomeError extends BaseError {
+ *   constructor(args?: unknown[], meta?: unknown) {
+ *     super(`Oops... Some error happen, at [%s], in [%s]`, args, meta);
+ *   }
+ * }
+ *
+ * const e = new MyAwesomeError([new Date(), 'core'], { hello: 'world!' });
+ *
+ * console.log(e.meta); // { hello: 'world!' }
+ * console.log(e.code); // MyAwesomeError
+ *
+ * throw e;
+ *           ^
+ * MyAwesomeError: Oops... Some error happen, at [2021-05-13T09:19:22.511Z], in [core]
+ *     at Object.<anonymous> (..core/packages/@biorate/errors/index.ts:28:11)
+ *     at Module._compile (internal/modules/cjs/loader.js:1138:30)
+ * ```
+ */
+export class BaseError extends Error {
+  #meta: any = null;
+
+  public constructor(
+    message: string,
+    args?: unknown[],
+    meta?: unknown,
+    ...options: unknown[]
+  ) {
+    // @ts-ignore: { cause } property added in 16.9.0
+    super(format(message, ...(args || [])), ...options);
+    this.#meta = meta;
+    this.name = this.constructor.name;
+    Error.captureStackTrace(this, this.constructor);
+  }
+
+  public get meta() {
+    return this.#meta;
+  }
+
+  public get code() {
+    return this.constructor.name;
+  }
+}
