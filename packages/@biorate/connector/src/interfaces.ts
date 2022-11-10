d1438d030b19daa9530e15836062bdd9466c4205
Leonid Levkin <llevkin@yandex.ru>
2022-11-10 15:56:55 +0300

    fix(connector): IConnector::get method to interface added

--- packages/@biorate/connector/src/interfaces.ts
+++ packages/@biorate/connector/src/interfaces.ts
@@ -7,4 +7,5 @@ export interface IConnector<C extends IConnectorConfig, T = any> {
   readonly connections: Map<string, T>;
   use(name: string): void;
   connection(name?: string): T;
+  get(name?: string): T;
 }
