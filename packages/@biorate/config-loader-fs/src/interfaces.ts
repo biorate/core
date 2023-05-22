bbc3d09a69392807f7a4599db4e9737dff02f516
Leonid Levkin <llevkin@yandex.ru>
2023-05-22 16:55:21 +0300

    fix: labels in prometheus

---
+++ packages/@biorate/config-loader-fs/src/interfaces.ts
@@ -0,0 +1,12 @@
+export enum ConfigLoaderVaultActions {
+  Merge = 'merge',
+  Download = 'download',
+}
+
+export type IConfigLoaderVaultOption = {
+  action: ConfigLoaderVaultActions.Merge;
+  path: string;
+  connection: string;
+  cache?: boolean;
+  directory?: string;
+};
