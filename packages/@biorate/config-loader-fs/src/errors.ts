923d9c005a2b4af6017416535867b953bf1f05b0
Leonid Levkin <llevkin@yandex.ru>
2023-05-23 09:43:12 +0300

    fix(config-loader-fs): text in ConfigLoaderFsFileNotLoadedError

--- packages/@biorate/config-loader-fs/src/errors.ts
+++ packages/@biorate/config-loader-fs/src/errors.ts
@@ -2,6 +2,6 @@ import { BaseError } from '@biorate/errors';
 
 export class ConfigLoaderFsFileNotLoadedError extends BaseError {
   public constructor(name: string, reason: string) {
-    super("ConfigLoaderFs: file [%s] - didn't merged, reason: [%s]", [name, reason]);
+    super("ConfigLoaderFs: file [%s] - didn't merge, reason: [%s]", [name, reason]);
   }
 }
