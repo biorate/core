3292d09e1bd5e610210dff2b8ffacab62d58182c
Leonid Levkin <llevkin@yandex.ru>
2023-08-10 16:41:30 +0300

    fix(config): npmignore

--- packages/@biorate/config/index.d.ts
+++ packages/@biorate/config/index.d.ts
@@ -1,15 +1,25 @@
 import { PropertyPath } from 'lodash';
 import { BaseError } from '@biorate/errors';
+import * as traverse from 'traverse';
 
 declare module '@biorate/config' {
   export class Config {
-    get<T = unknown>(path: PropertyPath, def?: T): T;
-    has(path: PropertyPath): boolean;
-    set(path: PropertyPath, value: unknown): void;
-    merge(data: unknown): void;
+    protected data: Record<string | symbol, unknown>;
+
+    protected template(value: string): string;
+
+    protected templatize(object: unknown): any;
+
+    public get<T = unknown>(path: PropertyPath, def?: T): T;
+
+    public has(path: PropertyPath): boolean;
+
+    public set(path: PropertyPath, value: unknown): void;
+
+    public merge(data: unknown): void;
   }
 
   export class UndefinedConfigPathError extends BaseError {
-    constructor(path: PropertyPath);
+    public constructor(path: PropertyPath);
   }
 }
