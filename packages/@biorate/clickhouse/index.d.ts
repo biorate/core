0d6333188912157d276848ccf3e7ff0b3312b13a
Leonid Levkin <llevkin@yandex.ru>
2023-09-19 18:30:33 +0300

    fix(clickhouse): query + insert methods types

--- packages/@biorate/clickhouse/index.d.ts
+++ packages/@biorate/clickhouse/index.d.ts
@@ -1,5 +1,7 @@
 import { Connector } from '@biorate/connector';
 import { IClickhouseConfig, IClickhouseConnection } from './src/interfaces';
+import { IInsertParams, IQueryParams } from './src';
+import { omit } from 'lodash';
 
 declare module '@biorate/clickhouse' {
   export class ClickhouseConnector extends Connector<
@@ -17,6 +19,10 @@ declare module '@biorate/clickhouse' {
     /**
      * @description Make a current query
      */
-    public query<T = unknown>(query: string): Promise<T[]>;
+    public query<T = unknown>(query: string, params?: IQueryParams): Promise<T[]>;
+    /**
+     * @description Make a insert
+     */
+    public insert(query: string, params?: IInsertParams): Promise<Object[]>;
   }
 }
