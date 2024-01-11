9ab3deb8c7c98491cb5baaa3a516cb2ec8690bed
Leonid Levkin <llevkin@yandex.ru>
2024-01-11 13:49:28 +0300

    feat: playwright allure step added

--- packages/@biorate/playwright/src/context.ts
+++ packages/@biorate/playwright/src/context.ts
@@ -1,6 +1,14 @@
+import { allure } from 'allure-playwright';
 import { Context as ContextCommon } from '@biorate/run-context';
 import { Scenario as ScenarioSymbol } from './symbols';
 
 export class Context extends ContextCommon {
   protected static metaKey = ScenarioSymbol;
+
+  protected static async runStep(step: () => void | Promise<void>) {
+    await allure.step(
+      step?.name?.replace?.('bound ', '') || 'unknown',
+      async () => await step(),
+    );
+  }
 }
