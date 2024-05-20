a198dc4f16c6363a613c248852aab650032b05d8
Leonid Levkin <llevkin@yandex.ru>
2024-05-20 17:44:01 +0300

    fix(playwright): tests

--- packages/@biorate/playwright/tests/scenarios/scenario1.ts
+++ packages/@biorate/playwright/tests/scenarios/scenario1.ts
@@ -3,19 +3,19 @@ import { expect, Scenario, step } from '../../src';
 export class SubScenario1 extends Scenario {
   @step()
   protected async step0() {
-    await this.page.goto('https://ya.ru/');
-    await expect(this.page).toHaveTitle(/Яндекс/);
+    await this.page.goto('https://google.com/');
+    await expect(this.page).toHaveTitle(/Google/);
   }
 }
 
 export class Scenario1 extends SubScenario1 {
   @step('Scenario1, test step 1')
   protected async step1() {
-    await this.page.goto('https://playwright.dev/');
+    await this.page.goto('https://google.com/');
   }
 
   @step()
   protected async step2() {
-    await expect(this.page).toHaveTitle(/Playwright/);
+    await expect(this.page).toHaveTitle(/Google/);
   }
 }
