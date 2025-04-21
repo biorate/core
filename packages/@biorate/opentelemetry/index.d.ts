dc27bf5c9e007eff1b830b2d190a70246a7ae83b
Leonid Levkin <llevkin@yandex.ru>
2025-04-21 17:01:25 +0300

    fix(opentelemetry): index.d.ts

--- packages/@biorate/opentelemetry/index.d.ts
+++ packages/@biorate/opentelemetry/index.d.ts
@@ -1,7 +1,7 @@
 declare module '@biorate/opentelemetry' {
   export * from '@opentelemetry/api';
 
-  export const scope = (version?: string, name?: string) => any;
+  export function scope(version?: string, name?: string): any;
 
-  export const span = (name?: string) => any;
+  export function span(name?: string): any;
 }
