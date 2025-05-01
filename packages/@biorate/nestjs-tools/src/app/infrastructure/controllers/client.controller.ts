7fbc15a74e22bb6d66f45bc106143ba905e0c5d5
Leonid Levkin <llevkin@yandex.ru>
2025-05-01 12:53:06 +0300

    feat(nestjs-common): domain added

--- packages/@biorate/nestjs-tools/src/app/infrastructure/controllers/client.controller.ts
+++ packages/@biorate/nestjs-tools/src/app/infrastructure/controllers/client.controller.ts
@@ -3,21 +3,21 @@ import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { GetLangUseCase, SetLangUseCase } from '../../application/service/client';
 import { GetLocalesDTO, PostLocalesDTO } from './dto';
 
-@ApiTags('Locale')
-@Controller('locale')
-export class LocalesController {
+@ApiTags('Client')
+@Controller('client')
+export class ClientController {
   public constructor(
     private readonly getLang: GetLangUseCase,
     private readonly setLang: SetLangUseCase,
   ) {}
 
-  @Get(':lang/:namespace')
+  @Get('locale/:lang/:namespace')
   @ApiOperation({ summary: 'Get locale' })
   protected get(@Param() param: GetLocalesDTO) {
     return this.getLang.execute(param.lang, param.namespace);
   }
 
-  @Post(':lang/:namespace')
+  @Post('locale/:lang/:namespace')
   @ApiOperation({ summary: 'Put locale' })
   protected async post(
     @Param() param: PostLocalesDTO,
--- packages/@biorate/nestjs-tools/src/app/infrastructure/controllers/index.ts
+++ packages/@biorate/nestjs-tools/src/app/infrastructure/controllers/index.ts
@@ -1,2 +1,2 @@
 export * from './metrics.controller';
-export * from './locale.controller';
+export * from './client.controller';
