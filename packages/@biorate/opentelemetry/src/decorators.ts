81059bb265d0e712fa598f1721cf13fdab056e3e
Leonid Levkin <leonid.levkin@lemanapro.ru>
2025-09-09 15:15:59 +0300

    feat(opentelemetry): nestjs fix

--- packages/@biorate/opentelemetry/src/decorators.ts
+++ packages/@biorate/opentelemetry/src/decorators.ts
@@ -15,37 +15,40 @@ export const span =
   (name?: string) =>
   (target: any, propertyKey: string, descriptor: PropertyDescriptor) => {
     const method = descriptor.value;
-    descriptor.value = function (...args: any[]) {
-      const tracer: Tracer = Reflect.getOwnMetadata(key, target.constructor);
-      if (!tracer) throw new OTELUndefinedTracerError(target.constructor.name);
-      return tracer.startActiveSpan(name ?? propertyKey, (span) => {
-        try {
-          span.setAttribute('class', target.constructor.name);
-          span.setAttribute('method', propertyKey);
-          span.setAttribute('arguments', stringify(args));
-          const result = method.apply(this, args);
-          if (result instanceof Promise)
-            return result
-              .then((result: unknown) => {
-                span.setAttribute('result', stringify(result));
-                return result;
-              })
-              .catch((e: unknown) => {
-                span.recordException(<Error>e);
-                span.setStatus({ code: SpanStatusCode.ERROR });
-                throw e;
-              })
-              .finally(() => span.end());
-          else span.setAttribute('result', stringify(result));
-          span.end();
-          return result;
-        } catch (e) {
-          span.recordException(<Error>e);
-          span.setStatus({ code: SpanStatusCode.ERROR });
-          span.end();
-          throw e;
-        }
-      });
+    const obj = {
+      [propertyKey]: function (...args: any[]) {
+        const tracer: Tracer = Reflect.getOwnMetadata(key, target.constructor);
+        if (!tracer) throw new OTELUndefinedTracerError(target.constructor.name);
+        return tracer.startActiveSpan(name ?? propertyKey, (span) => {
+          try {
+            span.setAttribute('class', target.constructor.name);
+            span.setAttribute('method', propertyKey);
+            span.setAttribute('arguments', stringify(args));
+            const result = method.apply(this, args);
+            if (result instanceof Promise)
+              return result
+                .then((result: unknown) => {
+                  span.setAttribute('result', stringify(result));
+                  return result;
+                })
+                .catch((e: unknown) => {
+                  span.recordException(<Error>e);
+                  span.setStatus({ code: SpanStatusCode.ERROR });
+                  throw e;
+                })
+                .finally(() => span.end());
+            else span.setAttribute('result', stringify(result));
+            span.end();
+            return result;
+          } catch (e) {
+            span.recordException(<Error>e);
+            span.setStatus({ code: SpanStatusCode.ERROR });
+            span.end();
+            throw e;
+          }
+        });
+      },
     };
+    descriptor.value = obj[propertyKey];
     copyMetadata(method, descriptor.value);
   };
