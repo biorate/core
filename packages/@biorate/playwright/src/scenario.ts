f0543d89c589534d46340ab82aba410be9eda320
Leonid Levkin <leonid.levkin@lemanapro.ru>
2025-11-17 15:36:41 +0300

    fix(playwright): types fix

--- packages/@biorate/playwright/src/scenario.ts
+++ packages/@biorate/playwright/src/scenario.ts
@@ -4,7 +4,7 @@ import { Page } from './';
 
 export abstract class Scenario extends ScenarioCommon {
   protected get page() {
-    return <Page>this.ctx.get<Page>('page');
+    return this.ctx.get<Page>('page');
   }
 }
 
